6de95e9b2c47cb123b5b6ff96f72c4be9f1d5f35
Axel Moncada <tatomoncada@gmail.com>
2021-12-28 23:04:35 -0500

    Clase javascript 28 diciembre2021

---
+++ Pruebas js.js
@@ -0,0 +1,41 @@
+valores.js
+
+    Numero = 40
+    Texto = "Diego de granda"
+    Bulliano = true , false 
+    vacios= null , undefined 
+    objetos= 
+    array= {} []
+
+Variables=
+ var nombre = "Axel Moncada"
+
+    // Declarar 
+    var edad;
+    // Inicializar
+    edad = 30; 
+
+    edad
+
+    var elementos = ["computadores","celular","cocina"];
+    var persona = {
+        nombres: "diego",
+        edad:30
+    }
+
+    persona;
+
+//FUNCIONES
+
+    //DECLARATIVAS
+        function miFuncion() {
+            return 3;
+        }
+        miFuncion();
+        
+    //EXPRECION 
+        var miFuncion = function(){
+            return a + b;
+        }
+
+        miFuncion();
